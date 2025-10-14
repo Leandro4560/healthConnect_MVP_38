840106e08dbeac887ea3ba9e8f757ac3a571e751
Alejandro <navarro.alejandro@outlook.es>
2025-10-13 21:11:25 -0600

    feat: finalizar formulario de registro, agregar variables a colores y font family Inter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
           <Route element={<h1 className="text-9xl"> Not Found!</h1>} />
         </Routes>
       </BrowserRouter>

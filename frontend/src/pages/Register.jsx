f96fd6a067e88d7ee494abf7656ba210679a6eab
Alejandro <navarro.alejandro@outlook.es>
2025-10-15 17:08:18 -0600

    style: actualizar estilos

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo/Logo.svg";
 import RegisterForm from "../components/user-register/RegisterForm";
 
 const Register = () => {
   return (
     <div className="h-dvh flex flex-col justify-evenly items-center bg-gradient-to-r from-primary-700 to-secondary">
-      <div className="w-80 h-[650px] bg-primary-100 rounded-2xl">
+      <div className="w-80 h-[650px] md:w-[650px] md:h-[730px] md:flex flex-col justify-evenly  bg-primary-100 rounded-2xl">
         <div className="h-56 flex flex-col justify-around items-center">
           <figure className="w-28 h-28 ">
             <img
@@ -13,8 +14,13 @@ const Register = () => {
               className="w-full h-full rounded-b-4xl object-contain"
             />
           </figure>
-          <h1 className="text-2xl">Te damos la bienvenida</h1>
-          <h3 className="text-2xl">¡Registrate!</h3>
+          <h1 className="text-2xl md:text-3xl">Registrate</h1>
+          <div className="flex">
+            <p className="pr-3">¿Ya tienes cuenta?</p>
+            <Link to="/login" className="font-medium">
+              Inicia sesión
+            </Link>
+          </div>
         </div>
         <RegisterForm />
       </div>

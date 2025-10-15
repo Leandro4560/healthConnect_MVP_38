2b1386ea5c21eab5fae6f113293cacd665cb0c35
Alejandro <navarro.alejandro@outlook.es>
2025-10-15 15:47:45 -0600

    feat: login terminado

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { userAuth } from "../context/Authcontext";
+
 const Home = () => {
-    return <h1 className="text-2xl">Bienvenido</h1>
+  const { user, signOut } = userAuth();
+  return (
+    <div>
+      <h1 className="text-2xl">Bienvenido: {user.full_name}</h1>
+      <button onClick={signOut}>Cerrar sesión</button>
+    </div>
+  );
 };
 
 export default Home;

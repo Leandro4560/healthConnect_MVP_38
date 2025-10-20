f3c360a5374f583ff362e8bf0d91c9fee864487a
Leandro74377 <leandrobaque4@gmail.com>
2025-10-20 18:15:59 -0500

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 from pydantic import Field
-# Nota: Usamos BaseSettings de pydantic_settings
+
 from pydantic_settings import BaseSettings, SettingsConfigDict 
 
 

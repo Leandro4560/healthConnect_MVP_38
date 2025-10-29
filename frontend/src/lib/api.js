0c272b2f2c095db4deeffb5d7565b33ecec2e08f
fernandobald <fernandobalda41@gmail.com>
2025-10-29 02:51:00 +0000

    actualizacion

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -6,3 +6,16 @@ export const api = axios.create({
   baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
 });
+
+export async function apiFetch(path, options = {}) {
+  const url = API_URL.replace(/\/+$/,"") + "/" + path.replace(/^\/+/,"");
+  const headers = { "Content-Type":"application/json", ...(options.headers||{}) };
+  const resp = await fetch(url, {...options, headers});
+  if (!resp.ok) {
+    const body = await resp.json().catch(()=>({}));
+    const err = new Error(body.detail || resp.statusText || "Error");
+    err.response = body;
+    throw err;
+  }
+  return resp.json().catch(()=>({}));
+}
